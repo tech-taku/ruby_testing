706519b8032ca528be629ec3c2ba2df18e2d055e
tech-taku <taku.map25@gmail.com>
2024-06-12 22:25:21 +0200

    rspec tests for the drinks class

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -89,15 +89,18 @@ end
 describe Drink do
   describe '#initialize' do
     context 'when type is specified and ounces is default' do
+      subject(:my_soda) { described_class.new('Cola', 16) }
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
 
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(my_soda.type).to eql('Cola')
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(my_soda.ounces).to eql(16)
       end
     end
   end
@@ -106,18 +109,20 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+      subject(:my_chibuku) { described_class.new('masese', 20) }
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(my_chibuku).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
-
+      subject(:my_super) {described_class.new('yorghut yabab', 5)}
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(my_super).not_to be_full
       end
     end
   end
